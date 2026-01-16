8797cf8fece4698887c3c33fce41fb8e34fb9a92
MatheusMendes <matheus.mendes@inpasa.com.br>
2026-01-16 13:46:58 -0400

    Update torre_resfriamento.py

--- torre_resfriamento.py
+++ torre_resfriamento.py
@@ -149,10 +149,8 @@ st.markdown("""
         margin: 0 !important;
         padding: 2px 0;
         opacity: 0.7;
-        background-color: #RRGGBBAA;
     }
     .flow-diagram {
-        background-color: #RRGGBBAA;
         padding: 0 !important;
         border-radius: 20px;
         margin: 0 !important;
